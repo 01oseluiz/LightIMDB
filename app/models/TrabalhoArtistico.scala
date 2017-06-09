ef8682a34c991f56095dc27da31773daf29c1480
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2017-06-09 11:47:46 -0300

    Adição da classe avaliação e de sua classe DAO.

--- app/models/TrabalhoArtistico.scala
+++ app/models/TrabalhoArtistico.scala
@@ -8,12 +8,14 @@ import javax.inject.Inject
 import play.api.db.Database
 
 case class TrabalhoArtistico(id: Int, tipo: Int, titulo: String, autor: String, genero: String, ano: Int, origem: String,
-descricao: String) {
+descricao: String, duracao: Int) {
 
   assert(id > 0)
   assert(tipo > 0)
   assert(ano > 0)
   assert(ano < 10000)
+  assert(duracao > 0)
+  assert(duracao < 1000)
 
 }
 
@@ -22,19 +24,20 @@ class TrabalhoArtisticoDAO @Inject() (database: Database) {
 
   def salvar(trabalho: TrabalhoArtistico) = database.withConnection { implicit connection =>
     val id: Option[Long] = SQL(
-      """INSERT INTO TB_TRAB_ART(TB_TIPO_id, titulo, autor, genero, ano, origem, descricao)
-            values ({tipo}, {titulo}, {autor}, {genero}, {ano}, {origem}, {descricao})""")
+      """INSERT INTO TB_TRAB_ART(TB_TIPO_id, titulo, autor, genero, ano, origem, descricao, duracao)
+            values ({tipo}, {titulo}, {autor}, {genero}, {ano}, {origem}, {descricao}, {duracao})""")
       .on('tipo -> trabalho.tipo,
         'titulo -> trabalho.titulo,
         'autor -> trabalho.autor,
         'genero -> trabalho.genero,
         'ano -> trabalho.ano,
         'origem -> trabalho.origem,
-        'descricao -> trabalho.descricao).executeInsert()
+        'descricao -> trabalho.descricao,
+        'duracao -> trabalho.duracao).executeInsert()
   }
 
   def listar = database.withConnection { implicit connection =>
-    SQL"SELECT TB_TIPO_id AS tipo, titulo, autor, genero, ano, origem, descricao FROM TB_FILME".as(parser.*)
+    SQL"SELECT TB_TIPO_id AS tipo, titulo, autor, genero, ano, origem, descricao, duracao FROM TB_FILME".as(parser.*)
   }
 
 }
